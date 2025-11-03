94d4b6f90034f3d631bf85f93c6f2839a3f40da9
eitan <eitanluc@gmail.com>
2025-11-03 17:49:27 -0300

    ejercicio 3 guia 10 hechoo

--- Ejercicios/Python/Guia_10/ejer_3.py
+++ Ejercicios/Python/Guia_10/ejer_3.py
@@ -12,6 +12,38 @@
 #  elementos ”perro” o ”gato”.}
 #  asegura: {Si hay m´as de una subsecuencia de tama˜no m´aximo, res tiene el ´ındice de la primera.}
 #  }
+def subsecuencias(tipos_pacientes_atendidos: list[str]) -> list[list[str]]:
+    subsecuencia: list[list[tuple[str, int]]] = []
+    aux: list[tuple[str, int]] = []
+    for i in range(0, len(tipos_pacientes_atendidos)):
+        if tipos_pacientes_atendidos[i] == "perro" or tipos_pacientes_atendidos[i] == "gato":
+            aux.append((tipos_pacientes_atendidos[i], i))
+        else:
+            subsecuencia.append(aux)
+            aux = []
+    if aux != []:
+        subsecuencia.append(aux)
+    return subsecuencia
 
-def subsecuencia_mas_larga(tipos_pacientes_atendidos: str)-> int:
-    return 12
+def elemento_mas_largo(subsecuencia: list[list[str]])-> int:
+    res: int = len(subsecuencia[0])
+    for lista in subsecuencia:
+        if res < len(lista):
+            res =  len(lista)
+    return res
+
+
+def subsecuencia_mas_larga(tipos_pacientes_atendidos: list[str])-> int:
+    subsecuencia: list[list[str]] = subsecuencias(tipos_pacientes_atendidos)
+    tamaño_de_subsecuencia_mas_larga: int = elemento_mas_largo(subsecuencia)
+    
+    for e in subsecuencia:
+        if tamaño_de_subsecuencia_mas_larga == len(e):
+            return e[0][1]
+
+
+
+tipos_pacientes_atendidos: list[str] = ["gato","toro","perro","perro","iguana","gato","perro"]
+
+print(subsecuencias(tipos_pacientes_atendidos))
+print(subsecuencia_mas_larga(tipos_pacientes_atendidos))
